1d15104d582bb8e3ac96a7cb390c3fbdebd5eefc
sok <asdrrv@gmail.com>
2024-10-28 23:12:27 +0400

    Lab 4 task 1

---
+++ lab2/src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -0,0 +1,38 @@
+package concurrent;
+
+import functions.LinkedListTabulateFunction;
+import functions.TabulatedFunction;
+import functions.UnitFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyingTaskExecutor {
+    public static void main(String[] args) {
+        TabulatedFunction function = new LinkedListTabulateFunction( new UnitFunction(), 1,1000,1000);
+
+        //Создание листа потоков
+        List<Thread> threads = new ArrayList<>();
+
+        // Запуск 10 потоков, выполняющих задачу умножения
+        for (int i = 0; i < 10; i++) {
+            MultiplyingTask task = new MultiplyingTask(function);
+            Thread thread = new Thread(task);
+            threads.add(thread);
+        }
+
+        // Старт всех потоков
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        while (!threads.isEmpty()) {
+            threads.removeIf(thread -> !thread.isAlive());
+        }
+
+
+
+
+        System.out.println(function);
+    }
+}
