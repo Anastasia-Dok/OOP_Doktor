e71a8dbe0dfa38c305435bb46dffb6fad570bf56
sok <asdrrv@gmail.com>
2024-11-19 22:48:05 +0400

    Lab 5 task 1

--- lab2/src/main/java/database/repositories/PointRepository.java
+++ lab2/src/main/java/database/repositories/PointRepository.java
@@ -4,9 +4,11 @@ package database.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import database.entity.MathFunctionsEntity;
 import database.entity.PointEntity;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PointRepository extends JpaRepository<PointEntity, Long> {
     List<PointEntity> findByFunction(MathFunctionsEntity function);
 }
